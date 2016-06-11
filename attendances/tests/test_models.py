350172f4762b64327013ba4a94ec3034875c5608
William Cuba <wcubasalas@gmail.com>
2016-06-11 18:32:46 -0500

    correction in name of course

--- attendances/tests/test_models.py
+++ attendances/tests/test_models.py
@@ -12,5 +12,5 @@ class TestAttendance:
     @pytest.mark.django_db
     def test_get_create_attendance(self):
         student = Student.objects.create(name="John")
-        course = Course.objects.create(name="John")
+        course = Course.objects.create(name="maths")
         Attendance.objects.create(student=student, course=course)

912e1721ded14e5ed67f89089b6a5dd3e0784c0d
William Cuba <wcubasalas@gmail.com>
2016-06-11 18:51:28 -0500

    view register according to new url more clear usage of form test for views passing

--- attendances/views.py
+++ attendances/views.py
@@ -3,9 +3,10 @@ from .forms import RegisterStudentListForm
 
 
 def register(request, course):
-    student_list_form = RegisterStudentListForm(course, request.user)
+    student_list_form = RegisterStudentListForm(course=course, professor=request.user)
     return render(request, 'register.html', {'form': student_list_form})
 
 
-def registered(request):
-    return render(request, 'registered.html')
+def registered(request, course):
+    students = []
+    return render(request, 'registered.html', {'students': students})

124bfd583554c5904be765d0306e4736d5b2e3c4
William Cuba <wcubasalas@gmail.com>
2016-06-11 13:31:34 -0500

    functional test use a precreated data test not passing

--- functional_tests/test_attendance.py
+++ functional_tests/test_attendance.py
@@ -2,6 +2,7 @@ from base import FunctionalTest
 from register_attendance_student_page import RegisterStudentPage
 from list_student_page import ListStudentPage
 from django.test.utils import override_settings
+from attendances.models import Course, Student
 
 
 class AttendanceTest(FunctionalTest):
@@ -9,11 +10,18 @@ class AttendanceTest(FunctionalTest):
     @override_settings(DEBUG=True)
     def test_see_list_of_students_of_course(self):
         # Given a database with students enrolled to courses
+        course = Course.objects.create(name="maths")
+        students_of_course = [
+            Student.objects.create(name="john"),
+            Student.objects.create(name="michael")
+        ]
+        course.students.add(*students_of_course)
+
         register_student_page = RegisterStudentPage(self.browser, root_uri=self.live_server_url)
 
         # I want to see the students avaible to register with course with id=1
         register_student_page.get("/attendances/register/1")
-        register_student_page.register("John")
+        register_student_page.register("john")
         list_student_page = ListStudentPage(self.browser, root_uri=self.live_server_url)
 
         # I want to see the list of my students

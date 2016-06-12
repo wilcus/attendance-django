daa18b7c9799123d03103ca2b4270ef6219a9d44
William Cuba <wcubasalas@gmail.com>
2016-06-11 20:31:21 -0500

    better message when a student is not found

--- functional_tests/register_attendance_student_page.py
+++ functional_tests/register_attendance_student_page.py
@@ -6,6 +6,8 @@ class RegisterStudentPage(PageObject):
     submit_button = PageElement(name="submit")
 
     def register(self, student):
-        student_chekbox = [checkbox for checkbox in self.checkboxes if checkbox.get_attribute("value") == student][0]
+        student_chekboxes = [checkbox for checkbox in self.checkboxes if checkbox.get_attribute("value") == student]
+        assert len(student_chekboxes) > 0, "student not found in checkboxes"
+        student_chekbox = student_chekboxes[0]
         student_chekbox.click()
         self.submit_button.click()

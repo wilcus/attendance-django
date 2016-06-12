164eb784b80509754c60945cf8f2842be5de2ea6
William Cuba <wcubasalas@gmail.com>
2016-06-11 20:23:24 -0500

    pk for professor needed to filter students

--- attendances/tests/test_forms.py
+++ attendances/tests/test_forms.py
@@ -5,12 +5,12 @@ from unittest.mock import Mock, patch, ANY
 class TestForms:
     @patch('attendances.forms.Student')
     def test_create_form(self, mock_Student):
-        RegisterStudentListForm(course=ANY, professor=ANY)
+        RegisterStudentListForm(course=ANY, professor=Mock(pk=1))
 
     @patch('attendances.forms.Attendance')
     @patch('attendances.forms.Student')
     def test_save_attendances(self, mock_Student, mock_Attendance):
-        student_list_form = RegisterStudentListForm(course=ANY, professor=ANY)
+        student_list_form = RegisterStudentListForm(course=ANY, professor=Mock(pk=1))
         student_list_form.cleaned_data = {}
         students_to_register = [Mock(pk=1, name="john"), Mock(pk=2, name="michael")]
         student_list_form.cleaned_data['students'] = students_to_register

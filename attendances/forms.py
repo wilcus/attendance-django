f1d288212c5c5f4318c7a353547c472719d6eefd
William Cuba <wcubasalas@gmail.com>
2016-06-11 20:25:51 -0500

    fix queryset to get students from a course and a professor

--- attendances/forms.py
+++ attendances/forms.py
@@ -10,7 +10,8 @@ class RegisterStudentListForm(forms.Form):
         self.course = course
         self.fields['students'].queryset = Student.objects.filter(
             course=course,
-            professor=professor)
+            course__professors__pk__in=[professor.pk]
+        )
 
     def save(self):
         students = self.cleaned_data['students']

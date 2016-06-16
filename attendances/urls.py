b8b02d9c1d44fa2b6507f2c8d2027c096202a15e
William Cuba <wcubasalas@gmail.com>
2016-06-16 16:48:11 -0500

    adds courses views to urls

--- attendances/urls.py
+++ attendances/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import register, registered
+from .views import register, registered, courses
 
 urlpatterns = [
     url(r'^register/(\d+)$', register, name="register"),
     url(r'^registered/(\d+)$', registered, name="registered"),
+    url(r'^courses$', courses, name="courses"),
 ]

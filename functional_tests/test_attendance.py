9d1a589cb8c0bf9f4c232b3da8f42244cb399b7e
William Cuba <wcubasalas@gmail.com>
2016-06-17 18:09:17 -0500

    adds test to see list of registered student in form

--- functional_tests/test_attendance.py
+++ functional_tests/test_attendance.py
@@ -4,7 +4,7 @@ from list_student_page import ListStudentPage
 from course_list_page import CourseListPage
 from login_page import LoginPage
 from django.test.utils import override_settings
-from attendances.models import Course, Student
+from attendances.models import Course, Student, Attendance
 from django.contrib.auth import get_user_model, SESSION_KEY, BACKEND_SESSION_KEY, HASH_SESSION_KEY
 from django.contrib.sessions.backends.db import SessionStore
 from django.conf.global_settings import SESSION_COOKIE_NAME
@@ -50,6 +50,39 @@ class AttendanceTest(FunctionalTest):
         list_student_page.get("/attendances/registered/1")
         students_registered = list_student_page.students_registered
         self.assertIn(john.name, students_registered)
+
+    def test_see_list_of_registered_students_in_form(self):
+        # Given a database with students enrolled to courses
+        course = Course.objects.create(name="maths")
+        john = Student.objects.create(name="john")
+
+        students_of_course = [
+            john,
+            Student.objects.create(name="michael")
+        ]
+        course.students.add(*students_of_course)
+        professor = User.objects.create(username="george")
+        course.professors.add(professor)
+        Attendance.objects.create(course=course, student=john)
+
+        session = SessionStore()
+        session[SESSION_KEY] = professor.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session[HASH_SESSION_KEY] = professor.get_session_auth_hash()
+        session.save()
+        self.browser.get(self.live_server_url + "/fake/")
+        self.browser.add_cookie(dict(
+            name=SESSION_COOKIE_NAME,
+            value=session.session_key,
+            path='/',
+        ))
+
+        register_student_page = RegisterStudentPage(self.browser, root_uri=self.live_server_url)
+
+        # I want to see john student is checked in form
+        register_student_page.get("/attendances/register/{0}".format(course.pk))
+        checked_students = register_student_page.checked_students
+        self.assertIn(john.name, checked_students)
 
     def test_when_logged_go_to_course_list_page(self):
         # create a professor with courses

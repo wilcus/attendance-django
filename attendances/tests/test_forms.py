a03a5badadae3e337a9bef6be78687df22b78ed8
William Cuba <wcubasalas@gmail.com>
2016-06-20 18:04:24 -0500

    adds test to do not save already created attendance

--- attendances/tests/test_forms.py
+++ attendances/tests/test_forms.py
@@ -18,3 +18,16 @@ class TestForms:
         student_list_form.save()
 
         assert mock_Attendance.objects.create.call_count == len(students_to_register)
+
+    @patch('attendances.forms.Attendance')
+    @patch('attendances.forms.Student')
+    def test_do_not_save_attendances_when_is_already_saved(self, mock_Student, mock_Attendance):
+        student_list_form = RegisterStudentListForm(course_id=ANY, professor=ANY)
+        student_list_form.cleaned_data = {}
+        students_to_register = [Mock(pk=1, name="john"), Mock(pk=2, name="michael")]
+        student_list_form.cleaned_data['students'] = students_to_register
+        mock_Attendance.objects.filter.return_value.exists.return_value = True
+
+        student_list_form.save()
+
+        assert mock_Attendance.objects.create.call_count == 0

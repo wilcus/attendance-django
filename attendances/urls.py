72f8337a39d7eb31218b5bcf90d25e2f55f77d4f
William Cuba <wcubasalas@gmail.com>
2016-06-21 18:45:16 -0500

    add parameter date for url functional test passing

--- attendances/urls.py
+++ attendances/urls.py
@@ -3,6 +3,6 @@ from .views import register, registered, courses
 
 urlpatterns = [
     url(r'^register/(\d+)$', register, name="register"),
-    url(r'^registered/(\d+)$', registered, name="registered"),
+    url(r'^registered/(\d+)/(\d{4}-\d{2}-\d{2})$', registered, name="registered"),
     url(r'^courses$', courses, name="courses"),
 ]

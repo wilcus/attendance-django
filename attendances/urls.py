273ac1367628a18767c66b71dc8d8f28d7bc9efd
William Cuba <wcubasalas@gmail.com>
2016-06-23 21:55:53 -0500

    new url to get list of link of dates

--- attendances/urls.py
+++ attendances/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import register, registered, courses
+from .views import register, registered, courses, registered_dates
 
 urlpatterns = [
     url(r'^register/(\d+)$', register, name="register"),
     url(r'^registered/(\d+)/(\d{4}-\d{2}-\d{2})$', registered, name="registered"),
+    url(r'^registered-dates/(\d+)$', registered_dates, name="registered-dates"),
     url(r'^courses$', courses, name="courses"),
 ]
